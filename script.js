564172517b6a21c6a5ac10bbf762e89e738c92cf
CeferAbid <ceferabid.w@gmail.com>
2023-05-19 16:26:06 +0400

    created github-search-user project

--- script.js
+++ script.js
@@ -1,25 +1,20 @@
-"use strict";
+import { getUserProfil, toggleDarkMode } from "./helper.js";
 
 const modeIcon = document.querySelector(".tema--icon");
-const modeIconText = document.querySelector(".tema--text");
 const form = document.querySelector(".filter--form");
 const input = document.querySelector(".filter--input");
 
 // Init
-import { getUserProfil } from "./functions.js";
-
 let darkMode = false;
-modeIconText.textContent = "DARK";
 input.focus();
 
-// Coding
-if (localStorage.getItem("mode")) {
-  darkMode = JSON.parse(localStorage.getItem("mode"));
-  if (darkMode) {
-    document.body.classList.add("dark-mode");
-    modeIconText.textContent = "LIGHT";
-    modeIcon.src = "img/icon/icon-sun.svg";
-  }
+// sil !! ~~~~~~~~~
+getUserProfil("cefer-abid");
+
+// Toggle dark mode when reload
+if (localStorage.getItem("dark-mode")) {
+  darkMode = JSON.parse(localStorage.getItem("dark-mode"));
+  toggleDarkMode(darkMode);
 }
 
 form.addEventListener("submit", function (e) {
@@ -32,23 +27,8 @@ form.addEventListener("submit", function (e) {
   input.value = ``;
 });
 
-// DARK MODE
+// Toggle dark mode when reload
 modeIcon.addEventListener("click", function () {
-  if (!darkMode) {
-    document.body.classList.add("dark-mode");
-    modeIconText.textContent = "LIGHT";
-    modeIcon.src = "img/icon/icon-sun.svg";
-  }
-  if (darkMode) {
-    document.body.classList.remove("dark-mode");
-    modeIconText.textContent = "DARK";
-    modeIcon.src = "img/icon/icon-moon.svg";
-  }
   darkMode = !darkMode;
-  localStorage.setItem("mode", darkMode);
+  toggleDarkMode(darkMode);
 });
-
-// getUserProfil('jonasschmedtmann');
-// getUserProfil('octocat');
-
-// fsaf
